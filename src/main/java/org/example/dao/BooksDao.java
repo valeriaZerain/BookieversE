2fb8b1450adb3fe90b048c8d48eadb15742505d2
vale_ <valeria.zerain@gmail.com>
2024-03-05 17:58:17 -0400

    Primer commit

--- src/main/java/org/example/dao/BooksDao.java
+++ src/main/java/org/example/dao/BooksDao.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface BooksDao {
-    public List<Books> getBooks()
+    List<Books> getBooks()
             throws SQLException;
-    public List<Books> getBooksByAuthor(String author)
+    List<Books> getBooksByAuthor(String author)
             throws SQLException;
 
-    public List<Books> getBooksByText(String textSearch)
+    List<Books> getBooksByText(String textSearch)
             throws SQLException;
 
 }
